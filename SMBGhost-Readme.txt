2b3e4a40e524a922fe7fd888751806c95b3bb697
Ahmad Almorabea <ahmad@almorabea.net>
2020-03-12 23:46:47 +0300

    Commit Files v1

---
+++ SMBGhost-Readme.txt
@@ -0,0 +1,17 @@
+SMBGhost Workarounds Scanner for CVE-2020-0796 - SMBv3 unauthenticated RCE
+
+This scanner has the ability to detect and apply the workaround in case your system is not patched yet.
+The Scanner is looking for the vulrability in your system only.
+You should run this script as administrator.
+
+Libraries used in this project:
+
+* winreg 
+* platform, os
+* os
+* ctypes
+
+Usage: python3 SMBGhost-workarounds
+
+For more informaiton check:
+https://portal.msrc.microsoft.com/en-US/security-guidance/advisory/adv200005
---
+++ SMBv3-WorkaroundApplier.py
@@ -0,0 +1,37 @@
+# Date 03/12/2020 - March
+# CVE-2020-0796 (SMBGhost,CoronaBlue) Workaround Applier - Created by Ahmad Almorabea @almorabea
+# Description: SMBv3 "SMB 3.1.1" has an unauthenticated RCE vulnerability and it's critical - Remember WannaCry/Petya/NotPetya ?!
+
+from winreg import *
+import platform, os
+from ctypes import *
+
+print("Your system information as follows")
+print(platform.uname())
+Registry = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+path = "SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters"
+RawKey = OpenKey(Registry, path)
+name, value, type = EnumValue(RawKey, 9)
+
+try:
+    is_admin = os.getuid() == 0
+except AttributeError:
+    is_admin = windll.shell32.IsUserAnAdmin() != 0
+
+if value == 1:
+    print("Not Vulnerable")
+else:
+    if is_admin:
+        res = input("Your System is Vulnerable, You want to apply the workarounds? y/n  ")
+        if res == "y":
+            try:
+                registry_key = OpenKey(HKEY_LOCAL_MACHINE, path, 0, KEY_WRITE)
+                SetValueEx(registry_key, "DisableCompression", 0, REG_DWORD, 1)
+                CloseKey(registry_key)
+                exit("Workarounds applied!!")
+            except WindowsError as message:
+                print(message)
+        else:
+            exit("Good luck, you didn't choose yes so live with this option!")
+    else:
+        exit("Please run this script as administrator, Now you are not an admin to execute the commands!")
